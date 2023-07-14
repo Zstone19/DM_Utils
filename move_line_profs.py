0468c4c1fc621b4bd9f8c228edc9400a177d66a1
Zstone19 <stone28@illinois.edu>
2023-07-13 23:46:52 -0500

    Added script to move the fitted line profiles

---
+++ move_line_profs.py
@@ -0,0 +1,29 @@
+import glob
+import numpy as np
+
+import shutil
+
+
+def move_line_profs(res_dir, out_dir, lines_in, lines_out):
+    epoch_dirs = glob.glob(res_dir + 'epoch*')
+    epochs = [ int(d[-3:]) for d in epoch_dirs ]
+
+    for i, epoch in enumerate(epochs):
+        for j in range(len(lines_in)):
+            
+            in_file = epoch_dirs[i] + r'/' + lines_in[j] + '_profile.csv'
+            out_file = out_dir + lines_out[j] + '/' + 'epoch{:03d}.csv'.format(epoch)
+        
+            shutil.copyfile(in_file, out_file)
+            
+    return
+
+
+if __name__ == '__main__':
+    res_dir = '/data3/stone28/2drm/sdssrm/fit_res_mg2/rm160/'
+    out_dir = '/data3/stone28/2drm/sdssrm/line_profs2/rm160/'
+
+    lines_in = ['MgII_br']
+    lines_out = ['mg2']
+
+    move_line_profs(res_dir, out_dir, lines_in, lines_out)
