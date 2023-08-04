83ff4aaa050606e0fd86c3efdb1fe5cfb7f306c4
Zstone19 <stone28@illinois.edu>
2023-08-04 16:33:31 -0500

    Fixed __init__.py for specfit

--- src/dmutils/specfit/__init__.py
+++ src/dmutils/specfit/__init__.py
@@ -2,6 +2,8 @@ from .object import Object
 from .move_line_profs import move_line_profs
 from .host_contribution import *
 from .fe2_contribution import *
+from .fit_lines import *
 
 __all__ = ["dmutils.specfit.object", "dmutils.specfit.move_line_profs",
-           "dmutils.specfit.host_contribution", "dmutils.specfit.fe2_contribution"]
+           "dmutils.specfit.host_contribution", "dmutils.specfit.fe2_contribution",
+           "dmutils.specfit.fit_lines"]
