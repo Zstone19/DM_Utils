44d51e792a2bbd7d139767f7ab01401e32197f29
Zstone19 <stone28@illinois.edu>
2023-09-12 02:29:18 -0500

    Still fixing...

--- src/dmutils/plot_multiple.py
+++ src/dmutils/plot_multiple.py
@@ -208,7 +208,7 @@ def latex_table_mult(res_arr, res_names=None, output_fname=sys.stdout):
         for j, name in enumerate(res.bp.para_names['name']):
                         
             if name in names_tot:
-                print(name)
+                print(name, name == 'BLR model ln(Rblr)')
                 name_ind = np.argwhere( names_tot == name )[0]
                 
                 if name_ind in logparam_names:
