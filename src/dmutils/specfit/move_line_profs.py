e5724cd21128a80caccf4273c3f7f62dd6da7b46
Zstone19 <stone28@illinois.edu>
2023-09-30 03:43:40 -0500

    Fixed typo in moving profiles

--- src/dmutils/specfit/move_line_profs.py
+++ src/dmutils/specfit/move_line_profs.py
@@ -4,23 +4,13 @@ import shutil
 
 def move_line_profs(main_dir, rmid, line_name):
     
-    if line_name == 'ha':
-        line_in = 'Ha_br'
-    elif line_name == 'hb':
-        line_in = 'Hb_br'
-    elif line_name == 'mg2':
-        line_in = 'MgII_br'
-    elif line_name == 'c4':
-        line_in = 'CIV_br'
-    
-    
     rm_dir = main_dir + '/rm{:03d}/'.format(rmid)
     
     epoch_dirs = glob.glob(rm_dir + line_name + '/qsofit/epoch*')
     epochs = [ int(d[-3:]) for d in epoch_dirs ]
     
     for i in range(len(epochs)):
-        in_file = epoch_dirs[i] + r'/' + line_in + '_profile.csv'
+        in_file = epoch_dirs[i] + r'/raw_br_profile.csv'
         out_file = rm_dir + line_name + '/profile/epoch{:03d}.csv'.format(epochs[i])
         
         shutil.copyfile(in_file, out_file)
