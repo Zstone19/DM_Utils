56b25bffe6b93b78e289e5238168c548212e0b09
Zstone19 <stone28@illinois.edu>
2023-09-30 03:27:20 -0500

    Fixed typo in moving profiles

--- src/dmutils/specfit/move_line_profs.py
+++ src/dmutils/specfit/move_line_profs.py
@@ -16,7 +16,7 @@ def move_line_profs(main_dir, rmid, line_name):
     
     rm_dir = main_dir + '/rm{:03d}/'.format(rmid)
     
-    epoch_dirs = glob.glob(rm_dir + line_name + 'qsofit/epoch*')
+    epoch_dirs = glob.glob(rm_dir + line_name + '/qsofit/epoch*')
     epochs = [ int(d[-3:]) for d in epoch_dirs ]
     
     for i in range(len(epochs)):
