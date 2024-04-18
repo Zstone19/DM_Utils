fe08090f412ea0e1be37534844a1896910593460
Zstone19 <stone28@illinois.edu>
2024-04-18 16:27:33 -0500

    Added math file to __init__.py

--- src/dmutils/postprocess/__init__.py
+++ src/dmutils/postprocess/__init__.py
@@ -2,7 +2,8 @@ from .binlc import *
 from .lags import *
 from .result import Result
 from .modelgen import *
+from .modelmath import *
 from .multires import *
 
 __all__ = [ "dmutils.postprocess.binlc", "dmutils.postprocess.lags", "dmutils.postprocess.result", 
-            "dmutils.postprocess.modelgen", "dmutils.postprocess.multires"]
+            "dmutils.postprocess.modelgen", "dmutils.postprocess.modelmath", "dmutils.postprocess.multires"]
